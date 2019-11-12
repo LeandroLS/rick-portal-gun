0a811108a7125122fbebeccfadeb1e0254a0bff8
Leandro <llimas@outlook.com>
2019-11-11 23:43:45 -0300

    experimentos

---
+++ components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class App extends Component {
+    render() {
+        return (
+            <h1> olá </h1>
+        )
+    }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  'node': {
+    net: 'empty',
+    fs: 'empty'
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+};
