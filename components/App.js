f2845a24d21d4c3d4b42f9c265137a67c3114226
Leandro Lima <desenvolvedor02@adias.com.br>
2019-11-12 11:09:47 -0300

    minimamente funcional

--- components/App.js
+++ components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import RickAndMortyAPI from '../resources/RickMortyAPI';
 class App extends Component {
-    render() {
+    render(props) {
+        var character = this.props.character;
         return (
-            <h1> olá </h1>
+            <div>
+                {character}
+                <h1> Sou um componente </h1>
+            </div>
         )
     }
 }
-export default App;
+ReactDOM.render(<App character={'ola'}/>, document.getElementById('root'));
--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-import App from './components/App.js';
-import ReactDOM from 'react-dom';
-ReactDOM.render(<App />, document.getElementById('root'));
 const express = require('express');
 const app = express();
 const port = 3000;
 const characterRouter = require('./resources/character.js');
-const rickandmortyapi = require('./resources/rickmortyapi');
 app.set('view engine', 'pug');
 app.set('views', 'views');
 app.use(express.static('public'));
 app.use('/character', characterRouter);
 app.get('/', async (req, res) => {
-    res.render('teste'); return;
-    rickandmortyapi.getRandomChacters().then(characters => {
-        res.render('index', { characters : characters.data });
-    }).catch(err => {
-        res.render('index', { err : true });
-    });
+    res.render('teste');
 });
 console.log('rick-portal-gun listening port', port);
 app.listen('3000');
