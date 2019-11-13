3ef689f293ead4292df0a9fd4e7b4daed36242cb
Leandro Lima <desenvolvedor02@adias.com.br>
2019-11-13 09:32:06 -0300

    mensagem de erro funcional

--- components/App.js
+++ components/App.js
@@ -10,31 +10,48 @@ import Pagination from './Pagination';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {characters: []}
+        this.state = {characters: []};
     }
+
     async componentDidMount() {
         await RickAndMortyAPI.getRandomCharacters().then(characters => {
             this.setState({characters: characters.data});
-        })
+        }).catch((err) => {
+            this.setState({erro: true})
+        });
+
     }
+
+    hasError(){
+        if(this.state.erro){
+            return <h1 className="error-message"> Something is wrong with Rick And Morty API. Try again later. </h1>
+        } else {
+            return (
+                <div>
+                    <Form />
+                    <section className="api-data">
+                        {
+                            this.state.characters.map(character => {
+                                return (<Card image={character.image}
+                                name={character.name} 
+                                status={character.status} 
+                                gender={character.gender} 
+                                species={character.species}/>)
+                            })
+                        }
+                    </section>
+                    <Pagination />
+                </div>
+            )
+        }
+    }
+
     render(props) {
         return (
             <div>
                 <Header />
                 <Description />
-                <Form />
-                <section className="api-data">
-                    {
-                        this.state.characters.map(character => {
-                            return (<Card image={character.image}
-                            name={character.name} 
-                            status={character.status} 
-                            gender={character.gender} 
-                            species={character.species}/>)
-                        })
-                    }
-                    <Pagination pages={2}/>
-                </section>
+                {this.hasError()}
             </div>
         )
     }
